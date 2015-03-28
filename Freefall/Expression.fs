61bb044b02c23d5e16cf37bf3b9f4a1207483d3a
Don Cross <cosinekitty@gmail.com>
2015-03-28 19:02:32 -0400

    Starting a unit test console app.

---
+++ Freefall/Expression.fs
@@ -0,0 +1,10 @@
+﻿module Freefall.Expr
+
+type Number =
+    | Rational of int64 * int64     // a perfect ratio of 64-bit signed integers encoded as a tuple (numerator, denominator)
+    | Real of float                 // an inexact floating point value
+
+type Expression =
+    | Number of Number
+    | Unit of string                // a physical unit name like "kilogram" or "mile"
+    | Variable of string
---
+++ UnitTest/Program.fs
@@ -0,0 +1,9 @@
+﻿open Freefall.Expr
+
+let AlmostPi:Expression = Number(Real(3.14))
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" AlmostPi
+    0 // return an integer exit code
+
