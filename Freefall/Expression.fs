10d3754374e3ee9e539d89822f549c3596e451e4
Don Cross <cosinekitty@gmail.com>
2015-03-29 16:22:33 -0400

    Functions to determine the units of expressions.

--- Freefall/Expression.fs
+++ Freefall/Expression.fs
@@ -8,6 +8,26 @@ module Expr =
         | Real of float
         | Complex of float * float
 
+    let rec GreatestCommonDivisor (a:int64) (b:int64) =         // caller must ensure that a and b are both non-negative
+        if b = 0L then
+            if a = 0L then 1L else a
+        else
+            GreatestCommonDivisor b (a%b)
+
+    let rec MakeRationalPair (numer:int64) (denom:int64) =
+        if denom = 0L then 
+            failwith "Rational number cannot have zero denominator."
+        elif numer = 0L then
+            (0L,1L)
+        elif denom < 0L then
+            MakeRationalPair (-numer) (-denom)
+        else
+            let gcd = GreatestCommonDivisor (System.Math.Abs(numer)) denom
+            (numer/gcd, denom/gcd)
+
+    let MakeRational (numer:int64) (denom:int64) =
+        Rational(MakeRationalPair numer denom)
+
     let ConceptNames  = [ "mass"     ; "distance" ; "time"   ; "temperature" ; "substance" ; "current" ; "luminosity" ]
     let BaseUnitNames = [ "kilogram" ; "meter"    ; "second" ; "kelvin"      ; "mole"      ; "ampere"  ; "candela"    ]
     let NumDimensions = List.length ConceptNames
@@ -35,6 +55,9 @@ module Expr =
         | Sum of Expression list
         | Product of Expression list
         | Power of Expression * Expression
+
+    //-----------------------------------------------------------------------------------------------------
+    // Formatting - conversion of expressions to human-readable strings.
 
     let FormatNumber x =
         match x with
@@ -69,15 +92,24 @@ module Expr =
         | (_,"")  -> prefix
         | (_,_)   -> prefix + "*" + text
 
-    let FormatConcept namelist (Concept(powlist)) =
+    let FormatDimensions namelist (Concept(powlist)) =
         List.fold2 AccumDimension "" namelist powlist
+
+    let FormatConcept = FormatDimensions ConceptNames
+
+    let FormatUnits = FormatDimensions BaseUnitNames
+
+    let FormatOptConcept opt =
+        match opt with
+        | None -> "0"
+        | Some(concept) -> FormatConcept concept
 
     let FormatQuantity (PhysicalQuantity(scalar,concept)) =
         if IsZero scalar then
             "0"     // special case because zero makes all units irrelevant
         else
             let scalarText = FormatNumber scalar
-            let conceptText = FormatConcept BaseUnitNames concept
+            let conceptText = FormatDimensions BaseUnitNames concept
             if conceptText = "" then
                 scalarText
             elif scalarText = "1" then
@@ -100,3 +132,64 @@ module Expr =
         | [] -> ""
         | [single] -> FormatExpression single
         | first :: rest -> FormatExpression first + "," + FormatExprList rest
+
+    //-----------------------------------------------------------------------------------------------------
+    // Unit determination - verify that units are coherent and determine what they are.
+    // For example, sum(3*meter,4*second) should raise an exception because adding distance to time is illegal.
+    // However, sum(3*meter,4*meter) should be seen as distance units (expressible in meters).
+    // Returns an Option(Concept) because None is needed for 0*anything, which has no specific units.
+    // No other reason for returning None should be allowed;
+    // must throw an exception for any unit compatibility violation.
+
+    let AddExponentLists (alist:(int64 * int64) list) (blist:(int64 * int64) list) =
+        List.map2 (fun (a,b) (c,d) -> MakeRationalPair (a*d + c*b) (b*d)) alist blist
+
+    let rec ExpressionConcept expr =
+        match expr with
+        | Amount(PhysicalQuantity(number,concept)) -> if IsZero number then None else Some(concept)
+        | Variable(_,concept) -> Some(concept)
+        | Negative(arg) -> ExpressionConcept arg
+        | Reciprocal(arg) -> ReciprocalConcept arg
+        | Sum(terms) -> SumConcept terms
+        | Product(factors) -> ProductConcept factors
+        | Power(a,b) -> PowerConcept a b
+
+    and SumConcept terms =
+        match terms with 
+        | [] -> None        // sum() = 0, which has no specific units -- see comments above.
+        | first::rest -> 
+            let firstOptConcept = ExpressionConcept first
+            let restOptConcept = SumConcept rest
+            match (firstOptConcept, restOptConcept) with
+            | (None,None) -> None                   // 0+0 = 0, which has no specific units
+            | (Some(f),None) -> firstOptConcept     // x+0 = x with specific units
+            | (None,Some(r)) -> restOptConcept      // 0+y = y
+            | (Some(f),Some(r)) ->
+                if f <> r then
+                    failwith (sprintf "Incompatible units: cannot add %s and %s" (FormatConcept f) (FormatConcept r))
+                else
+                    firstOptConcept
+
+    and ProductConcept factors =
+        match factors with 
+        | [] -> Some(Dimensionless)     // product() = 1, which has dimensionless units
+        | first::rest ->
+            let firstOptConcept = ExpressionConcept first
+            let restOptConcept = ProductConcept rest
+            match (firstOptConcept, restOptConcept) with
+            | (None,_) -> None      // 0*y = 0, which has no definite units
+            | (_,None) -> None      // x*0 = 0, which has no definite units
+            | (Some(Concept(alist)),Some(Concept(blist))) ->
+                // Add respective exponents to yield the exponents of the product
+                Some(Concept(AddExponentLists alist blist))
+
+    and PowerConcept a b =
+        // FIXFIXFIX: plan - numerically reduce b, require it to be dimensionless rational.
+        failwith "Not yet implemented."
+
+    and ReciprocalConcept arg =
+        match ExpressionConcept arg with
+        | None -> None
+        | Some(Concept(dimlist)) -> 
+            // Take the reciprocal by negating each rational number in the list of dimensional exponents.
+            Some(Concept(List.map (fun (numer,denom) -> MakeRationalPair (-numer) denom) dimlist))
--- UnitTest/Program.fs
+++ UnitTest/Program.fs
@@ -4,11 +4,16 @@ let Unknown = Variable ("x", Dimensionless)
 
 let AlmostPi = Amount(PhysicalQuantity(Rational(22L,7L), Dimensionless))
 
-let OneNewton = Amount(PhysicalQuantity(Rational(1L,1L), Concept[(1L,1L); (1L,1L); (-2L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L)]))
+let Weight = Amount(PhysicalQuantity(Rational(3L,4L), Concept[(1L,1L); (1L,1L); (-2L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L)]))
+
+let WeightSquared = Product[Weight;Weight]
 
 [<EntryPoint>]
 let main argv = 
     printfn "AlmostPi = %s" (FormatExpression AlmostPi)
-    printfn "OneNewton = %s" (FormatExpression OneNewton)
+    printfn "OneNewton = %s" (FormatExpression Weight)
+    printfn "WeightSquared = %s" (FormatExpression WeightSquared)
+    printfn "concept(WeightSquared) = %s" (FormatOptConcept (ExpressionConcept WeightSquared))
+
     0 // return an integer exit code
 
