6060f0e3987f3edf76b266b01b26e32a96a2582d
Don Cross <cosinekitty@gmail.com>
2015-03-29 18:04:00 -0400

    Added functions to compare two expressions for being identical, allowing commutativity for sums and products.

--- Freefall/Expression.fs
+++ Freefall/Expression.fs
@@ -28,6 +28,12 @@ module Expr =
     let MakeRational (numer:int64) (denom:int64) =
         Rational(MakeRationalPair numer denom)
 
+    let AddExponentLists (alist:(int64 * int64) list) (blist:(int64 * int64) list) =
+        List.map2 (fun (a,b) (c,d) -> MakeRationalPair (a*d + c*b) (b*d)) alist blist
+
+    let SubtractExponentLists (alist:(int64 * int64) list) (blist:(int64 * int64) list) =
+        List.map2 (fun (a,b) (c,d) -> MakeRationalPair (a*d - c*b) (b*d)) alist blist        
+
     let ConceptNames  = [ "mass"     ; "distance" ; "time"   ; "temperature" ; "substance" ; "current" ; "luminosity" ]
     let BaseUnitNames = [ "kilogram" ; "meter"    ; "second" ; "kelvin"      ; "mole"      ; "ampere"  ; "candela"    ]
     let NumDimensions = List.length ConceptNames
@@ -36,7 +42,39 @@ module Expr =
         | Zero
         | Concept of (int64 * int64) list       // list must have NumDimensions elements, each representing a rational number for the exponent of that dimension
 
-    let Dimensionless = Concept (List.replicate NumDimensions (0L,1L))
+    // Functions to help build concepts from other concepts...
+
+    let MultiplyConcepts a b =
+        match (a,b) with
+        | (_,Zero) -> Zero
+        | (Zero,_) -> Zero
+        | (Concept(alist),Concept(blist)) -> Concept(AddExponentLists alist blist)
+
+    let DivideConcepts a b =
+        match (a,b) with
+        | (_,Zero) -> failwith "Cannot divide concept by 0."
+        | (Zero,_) -> Zero
+        | (Concept(alist),Concept(blist)) -> Concept(SubtractExponentLists alist blist)
+
+    // Handy concepts by name...
+
+    // A concept to represent any dimensionless quantity...
+    let Dimensionless       = Concept[(0L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L)]
+
+    // Fundamental base concepts...
+    let MassConcept         = Concept[(1L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L)]
+    let DistanceConcept     = Concept[(0L,1L); (1L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L)]
+    let TimeConcept         = Concept[(0L,1L); (0L,1L); (1L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L)]
+    let TemperatureConcept  = Concept[(0L,1L); (0L,1L); (0L,1L); (1L,1L); (0L,1L); (0L,1L); (0L,1L)]
+    let SubstanceConcept    = Concept[(0L,1L); (0L,1L); (0L,1L); (0L,1L); (1L,1L); (0L,1L); (0L,1L)]
+    let CurrentConcept      = Concept[(0L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L); (1L,1L); (0L,1L)]
+    let LuminosityConcept   = Concept[(0L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L); (1L,1L)]
+
+    // Compound concepts...
+
+    let SpeedConcept = DivideConcepts DistanceConcept TimeConcept           // speed = distance/time
+    let AccelerationConcept = DivideConcepts SpeedConcept TimeConcept       // accleration = speed/time
+    let ForceConcept = MultiplyConcepts MassConcept AccelerationConcept     // force = mass * acceleration
 
     // A physical quantity is a numeric scalar attached to a physical concept.
     type PhysicalQuantity = PhysicalQuantity of Number * PhysicalConcept
@@ -142,9 +180,6 @@ module Expr =
     // No other reason for returning None should be allowed;
     // must throw an exception for any unit compatibility violation.
 
-    let AddExponentLists (alist:(int64 * int64) list) (blist:(int64 * int64) list) =
-        List.map2 (fun (a,b) (c,d) -> MakeRationalPair (a*d + c*b) (b*d)) alist blist
-
     let rec ExpressionConcept expr =
         match expr with
         | Amount(PhysicalQuantity(number,concept)) -> if IsZero number then Zero else concept
@@ -173,14 +208,8 @@ module Expr =
 
     and ProductConcept factors =
         match factors with 
-        | [] -> Dimensionless     // product() = 1, which has dimensionless units
-        | first::rest ->
-            let firstOptConcept = ExpressionConcept first
-            let restOptConcept = ProductConcept rest
-            match (firstOptConcept, restOptConcept) with
-            | (Zero,_) -> Zero      // 0*y = 0, which has no definite units
-            | (_,Zero) -> Zero      // x*0 = 0, which has no definite units
-            | (Concept(alist),Concept(blist)) -> Concept(AddExponentLists alist blist)
+        | [] -> Dimensionless     // product() = 1, which has dimensionless units            
+        | first::rest -> MultiplyConcepts (ExpressionConcept first) (ProductConcept rest)
 
     and PowerConcept a b =
         // FIXFIXFIX: plan - numerically reduce b, require it to be dimensionless rational.
@@ -192,3 +221,85 @@ module Expr =
         | Concept(dimlist) -> 
             // Take the reciprocal by negating each rational number in the list of dimensional exponents.
             Concept(List.map (fun (numer,denom) -> MakeRationalPair (-numer) denom) dimlist)
+
+    //-----------------------------------------------------------------------------------------------------
+    // Identity tester : determines if two expressions have equivalent values.
+    // For example, sum(a,b,c) looks different from sum(b,c,a), but are identical.
+
+    let IsZeroNumberConceptPair number concept =
+        (concept = Zero) || (IsZero number)
+
+    let rec AreIdenticalNumbers a b =
+        match (a,b) with
+        | (Rational(anum,aden),Rational(bnum,bden)) -> anum*bden = bnum*aden
+        | (Rational(anum,aden),Real(br)) -> (float anum) = br*(float aden)
+        | (Rational(anum,aden),Complex(br,bi)) -> (bi = 0.0) && (AreIdenticalNumbers a (Real(br)))
+        | (Real(ar),Rational(_,_)) -> AreIdenticalNumbers b a
+        | (Real(ar),Real(br)) -> ar = br
+        | (Real(ar),Complex(br,bi)) -> (bi = 0.0) && (ar = br)
+        | (Complex(_,_),Rational(_,_)) -> AreIdenticalNumbers b a
+        | (Complex(_,_),Real(_)) -> AreIdenticalNumbers b a
+        | (Complex(ar,ai),Complex(br,bi)) -> (ar = br) && (ai = bi)
+
+    let rec AreIdentical a b =
+        match (a,b) with
+        | (Amount(PhysicalQuantity(aNumber,aConcept)), Amount(PhysicalQuantity(bNumber,bConcept))) -> 
+            AreIdenticalQuantities aNumber aConcept bNumber bConcept
+        | (Amount(_), _) -> false
+        | (Variable(aName,aConcept), Variable(bName,bConcept)) ->
+            (aName = bName) && (
+                (aConcept = bConcept) || 
+                failwith (sprintf "Mismatching variable %s concepts : %s and %s" aName (FormatConcept aConcept) (FormatConcept bConcept))
+            )
+        | (Variable(_), _) -> false
+        | (Negative(na),Negative(nb)) -> AreIdentical na nb
+        | (Negative(_), _) -> false
+        | (Reciprocal(ra),Reciprocal(rb)) -> AreIdentical ra rb
+        | (Reciprocal(_), _) -> false
+        | (Sum(aterms),Sum(bterms)) -> ArePermutedLists aterms bterms
+        | (Sum(_), _) -> false
+        | (Product(afactors),Product(bfactors)) -> ArePermutedLists afactors bfactors
+        | (Product(_), _) -> false
+        | (Power(abase,aexp),Power(bbase,bexp)) -> (AreIdentical abase bbase) && (AreIdentical aexp bexp)
+        | (Power(_,_), _) -> false
+
+    and AreIdenticalQuantities aNumber aConcept bNumber bConcept =
+        let aIsZero = IsZeroNumberConceptPair aNumber aConcept
+        let bIsZero = IsZeroNumberConceptPair bNumber bConcept
+        if aIsZero || bIsZero then
+            // If either is zero, then then both must be zero to match.
+            aIsZero = bIsZero
+        else
+            // Neither is zero, so we must match numbers and concepts both.
+            (aConcept = bConcept) && (AreIdenticalNumbers aNumber bNumber)
+
+    and ArePermutedLists alist blist =
+        if List.length alist <> List.length blist then
+            false   // cannot possibly match if the lists have different lengths (important optimization to avoid lots of pointless work)
+        else
+            match (alist, blist) with
+                | ([], []) -> true
+                | ([], _::_) -> failwith "impossible case 1"    // cannot happen because we already matched list lengths above
+                | (_::_, []) -> failwith "impossible case 2"    // cannot happen because we already matched list lengths above
+                | (afirst::arest, bfirst::brest) -> 
+                    // Try to find afirst in bterms.
+                    // If not found, immediately return false.
+                    // If found, cancel it with its buddy in bterms, yielding arest and bshorter, then recurse.
+                    match FindIdenticalInList afirst blist with
+                    | None -> false
+                    | Some(bshorter) -> ArePermutedLists arest bshorter
+            
+    // The following function searches for an element of list that
+    // is mathematically identical to expr.  If found, returns Some(shorter)
+    // where shorter is list with the identical element removed.
+    // Otherwise, returns None.
+    and FindIdenticalInList expr list =
+        match list with
+        | [] -> None
+        | first::rest -> 
+            if AreIdentical expr first then
+                Some(rest)
+            else
+                match FindIdenticalInList expr rest with
+                | None -> None
+                | Some(shorter) -> Some(first :: shorter)
--- UnitTest/Program.fs
+++ UnitTest/Program.fs
@@ -4,9 +4,16 @@ let Unknown = Variable ("x", Dimensionless)
 
 let AlmostPi = Amount(PhysicalQuantity(Rational(22L,7L), Dimensionless))
 
-let Weight = Amount(PhysicalQuantity(Rational(3L,4L), Concept[(1L,1L); (1L,1L); (-2L,1L); (0L,1L); (0L,1L); (0L,1L); (0L,1L)]))
+
+let Weight = Amount(PhysicalQuantity(Rational(3L,4L), ForceConcept))
 
 let WeightSquared = Product[Weight;Weight]
+
+let IdentityTest a b =
+    if not (AreIdentical a b) then
+        failwith (sprintf "IdentityTest FAILED: %s <> %s" (FormatExpression a) (FormatExpression b))
+    else
+        printfn "%s = %s" (FormatExpression a) (FormatExpression b)
 
 [<EntryPoint>]
 let main argv = 
@@ -14,6 +21,11 @@ let main argv =
     printfn "OneNewton = %s" (FormatExpression Weight)
     printfn "WeightSquared = %s" (FormatExpression WeightSquared)
     printfn "concept(WeightSquared) = %s" (FormatConcept (ExpressionConcept WeightSquared))
+
+    let ForceVar = Variable("F", ForceConcept)
+    let MyScalar = Amount(PhysicalQuantity(Real(7.28), Dimensionless))
+
+    IdentityTest (Product[AlmostPi;ForceVar;MyScalar]) (Product[MyScalar;AlmostPi;ForceVar])
 
     0 // return an integer exit code
 
