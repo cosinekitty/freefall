3e9cf4cdc908bce4d0ad59e978ba6fa32772a6e9
Don Cross <cosinekitty@gmail.com>
2015-03-29 11:13:31 -0400

    Starting to format expressions and units.

--- Freefall/Expression.fs
+++ Freefall/Expression.fs
@@ -8,19 +8,13 @@ module Expr =
         | Real of float
         | Complex of float * float
 
-    // Based on the 7 SI base units:
-    // http://www.npl.co.uk/reference/measurement-units/
-    // [0] mass         : kilogram
-    // [1] distance     : meter
-    // [2] time         : second
-    // [3] temperature  : kelvin
-    // [4] substance    : mole
-    // [5] current      : ampere
-    // [6] luminosity   : candela
-    let NumDimensions = 7
+    let ConceptNames  = [ "mass"     ; "distance" ; "time"   ; "temperature" ; "substance" ; "current" ; "luminosity" ]
+    let BaseUnitNames = [ "kilogram" ; "meter"    ; "second" ; "kelvin"      ; "mole"      ; "ampere"  ; "candela"    ]
+    let NumDimensions = List.length ConceptNames
+
     type PhysicalConcept = Concept of (int64 * int64) list       // list must have 7 elements, each representing a rational number - see comments above
 
-    let Dimensionless = Concept (List.replicate NumDimensions (0L,0L))
+    let Dimensionless = Concept (List.replicate NumDimensions (0L,1L))
 
     // A physical quantity is a numeric scalar attached to a physical concept.
     type PhysicalQuantity = PhysicalQuantity of Number * PhysicalConcept
@@ -28,5 +22,60 @@ module Expr =
     let Unity = PhysicalQuantity(Rational(1L,1L), Dimensionless)
 
     type Expression =
-        | Quantity of PhysicalQuantity
-        | Variable of string
+        | Amount of PhysicalQuantity
+        | Variable of string * PhysicalConcept      // (name, units)
+        | Negative of Expression
+        | Reciprocal of Expression
+        | Sum of Expression list
+        | Product of Expression list
+        | Power of Expression * Expression
+
+    let FormatNumber x =
+        match x with
+        | Rational(numer,denom) ->
+            if denom = 0L then
+                failwith "Rational number had zero denominator."
+            elif denom = 1L then
+                numer.ToString()
+            else
+                numer.ToString() + "/" + denom.ToString()
+        | Real re -> re.ToString()
+        | Complex(re,im) -> "(" + re.ToString() + "," + im.ToString() + ")"
+
+    let FormatDimension prefix name (numer,denom) =
+        if numer = 0L then
+            prefix      // this dimension does not contribute
+        elif numer = 1L && denom = 1L then
+            prefix + "*" + name
+        elif denom = 1L then
+            prefix + "*" + name + "^" + numer.ToString()
+        else
+            prefix + "*" + name + "^(" + numer.ToString() + "/" + denom.ToString() + ")"
+
+    let FormatConcept namelist (Concept(powlist)) =
+        List.fold2 FormatDimension "" namelist powlist
+
+    let FormatQuantity (PhysicalQuantity(scalar,concept)) =
+
+        let scalarText = FormatNumber scalar
+        let conceptText = FormatConcept BaseUnitNames concept
+        if conceptText = "" then
+            scalarText
+        else
+            scalarText + "*" + conceptText
+
+    let rec FormatExpression expr =
+        match expr with
+        | Amount quantity -> FormatQuantity quantity
+        | Variable(name,_) -> name
+        | Negative arg -> "neg(" + FormatExpression arg + ")"
+        | Reciprocal arg -> "recip(" + FormatExpression arg + ")"
+        | Sum terms -> "sum(" + FormatExprList terms + ")"
+        | Product factors -> "prod(" + FormatExprList factors + ")"
+        | Power(a,b) -> "pow(" + FormatExpression a + "," + FormatExpression b + ")"
+
+    and FormatExprList list =
+        match list with
+        | [] -> ""
+        | [single] -> FormatExpression single
+        | first :: rest -> FormatExpression first + "," + FormatExprList rest
--- UnitTest/Program.fs
+++ UnitTest/Program.fs
@@ -1,12 +1,12 @@
 ﻿open Freefall.Expr
 
-let Unknown = Variable "x"
+let Unknown = Variable ("x", Dimensionless)
 
-let AlmostPi = PhysicalQuantity(Rational(22L,7L), Dimensionless)
+let AlmostPi = Amount(PhysicalQuantity(Rational(22L,7L), Dimensionless))
 
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" AlmostPi
+    printfn "AlmostPi = %A = %s" AlmostPi (FormatExpression AlmostPi)
     0 // return an integer exit code
 
