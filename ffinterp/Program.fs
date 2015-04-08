75e23ecb91a93df3b56685179b41c6e10608155a
Don Cross <cosinekitty@gmail.com>
2015-04-08 11:26:34 -0400

    Added interactive interpreter when placeholder filename '-' is encountered.

--- ffinterp/Program.fs
+++ ffinterp/Program.fs
@@ -6,13 +6,51 @@ open Freefall.Stmt
 open Freefall.Parser
 open Freefall.Intrinsic
 
-let ExecuteFile context filename =
-    let mutable scan = TokenizeFile filename
+let PrintTokenDiagnostic token =
+    printfn "Near '%s' @ col %d" token.Text token.ColumnNumber
+    match token.Origin with
+    | None -> ()
+    | Some({Filename=fn; LineNumber=ln;}) -> printfn "File %s [line %d]" fn ln
+
+let ExecuteStatements context tokenlist =
+    let mutable scan = tokenlist
     while MoreTokensIn scan do
         let statement, scan2 = ParseStatement scan
         printfn "Statement: %s" (FormatStatement statement)
         ExecuteStatement context statement
         scan <- scan2
+
+let ExecuteFile context filename =
+    if filename = "-" then
+        // Placeholder for a console session instead of an input file.
+        printf "Freefall> "
+        let mutable line = System.Console.ReadLine()
+        let mutable lineNumber = 0
+        while line <> null do
+            lineNumber <- 1 + lineNumber
+            try
+                TokenizeLine line |> ExecuteStatements context
+            with 
+                | SyntaxException(token,message) ->
+                    printfn "SYNTAX ERROR : %s" message
+                    PrintTokenDiagnostic token
+
+                | ExpressionException(expr,message) ->
+                    printfn "Error in subexpression '%s': %s" (FormatExpression expr) message
+                    match PrimaryToken expr with
+                    | None -> ()
+                    | Some(token) -> PrintTokenDiagnostic token
+
+                | UnexpectedEndException(None) ->
+                    printfn "Syntax error: unexpected end of input"
+
+                | UnexpectedEndException(Some(filename)) ->
+                    printfn "Syntax error: unexpected end of file '%s'" filename
+
+            printf "Freefall> "
+            line <- System.Console.ReadLine()
+    else
+        TokenizeFile filename |> ExecuteStatements context
 
 let MyAssignmentHook (targetName:option<string>) (refIndex:int) (assignedExpr:Expression) =
     match targetName with
@@ -20,12 +58,6 @@ let MyAssignmentHook (targetName:option<string>) (refIndex:int) (assignedExpr:Ex
     | Some(name) -> printf "%s := " name
     printfn "#%d := %s" refIndex (FormatExpression assignedExpr)
     printfn ""
-
-let PrintTokenDiagnostic token =
-    printfn "Near '%s' @ col %d" token.Text token.ColumnNumber
-    match token.Origin with
-    | None -> ()
-    | Some({Filename=fn; LineNumber=ln;}) -> printfn "File %s [line %d]" fn ln
 
 let MyProbeHook (expr:Expression) (range:NumericRange) =
     printfn "PROBE(expr ): %s" (FormatExpression expr)
