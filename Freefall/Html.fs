54a1f389bd8a643227626d9b863c461d0a49d268
Don Cross <cosinekitty@gmail.com>
2015-04-19 19:02:17 -0400

    First working version of html+MathJax saver.

--- Freefall/Html.fs
+++ Freefall/Html.fs
@@ -7,7 +7,26 @@ open Freefall.Stmt
 open Freefall.Parser
 open Freefall.Latex
 
-let HtmlSave context filename =
+let HtmlPrefix = """<!DOCTYPE html>
+<html>
+<head>
+<title>Freefall</title>
+<script type="text/x-mathjax-config">
+MathJax.Hub.Config({
+  TeX: { equationNumbers: { autoNumber: "AMS" } }
+});
+MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
+</script>
+<script src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML"></script>
+</head>
+<body>
+"""
+
+let HtmlSave (context:Context) filename =
     use output = File.CreateText(filename)
-    output.WriteLine("<html>")
-    output.WriteLine("</html>")
+    output.WriteLine(HtmlPrefix)
+    for equation in context.NumberedExpressionList do
+        output.WriteLine("\\begin{equation}")
+        output.WriteLine(FormatLatex context equation)
+        output.WriteLine("\\end{equation}")
+    output.WriteLine("</body></html>")
